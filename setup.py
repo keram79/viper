66ed8a34a78b8c6a84d4c9ed4b58fecaab734bc2
Robert Habermann <mail@rhab.de>
2017-05-03 04:15:04 -0700

    update setup.py and add Manifest

--- setup.py
+++ setup.py
@@ -13,10 +13,32 @@ setup(
     author_email='nex@nex.sx',
     description="Binary Analysis & Management Framework",
     url='http://viper.li',
-    license='BSD 3-Clause',
 
     scripts=['viper-cli', 'viper-api', 'viper-web', 'viper-update'],
     packages=find_packages(),
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
+
+    # BSD 3-Clause License:
+    # - http://choosealicense.com/licenses/bsd-3-clause
+    # - http://opensource.org/licenses/BSD-3-Clause
+    license='BSD 3-Clause',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Topic :: Security',
+
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+
+        'Operating System :: POSIX :: Linux',
+    ],
+
+    keywords='binary analysis management malware research',
 )
